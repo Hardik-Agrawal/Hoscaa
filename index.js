df6108078cbaa605cf040a7f36eadd350470f191
Hardik-Agrawal <hardik1509agrawal@gmail.com>
2019-06-11 09:08:38 +0530

    Added loader

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let banner = document.querySelector(".banner");
+
+window.addEventListener("load", function() {
+  const loader = document.querySelector(".loader");
+  loader.className += " hidden";
+});
+
 $(document).ready(function() {
   $(".dropdown").on("show.bs.dropdown", () => {
     //Banner fade IN
-    {
-      banner.style.opacity = 0;
-      let opacity = Number(
-        window.getComputedStyle(banner).getPropertyValue("opacity")
-      );
-      let intervalID = setInterval(fadeIn, 100);
-      function fadeIn() {
-        if (opacity < 1) {
-          opacity = opacity + 0.1;
-          banner.style.opacity = opacity;
-        } else {
-          clearInterval(intervalID);
-        }
+    let opacity = Number(
+      window.getComputedStyle(banner).getPropertyValue("opacity")
+    );
+    let intervalID = setInterval(fadeIn, 100);
+    function fadeIn() {
+      if (opacity < 1) {
+        opacity = opacity + 0.1;
+        banner.style.opacity = opacity;
+      } else {
+        clearInterval(intervalID);
       }
     }
-
-    //Club Items
-    
   });
 
   $(".dropdown").on("hide.bs.dropdown", () => {
