c43828bf46b7624503e9ce2780fcd42bfe79eef5
Hardik-Agrawal <hardik1509agrawal@gmail.com>
2019-07-08 16:51:12 +0530

    new menu

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-let banner = document.querySelector(".banner");
-
-window.addEventListener("load", function() {
-  const loader = document.querySelector(".loader");
-  loader.className += " hidden";
-});
-
 $(document).ready(function() {
   $(".dropdown").on("show.bs.dropdown", () => {
-    //Banner fade IN
-    let opacity = Number(
-      window.getComputedStyle(banner).getPropertyValue("opacity")
-    );
-    let intervalID = setInterval(fadeIn, 100);
-    function fadeIn() {
-      if (opacity < 1) {
-        opacity = opacity + 0.1;
-        banner.style.opacity = opacity;
+    var intervalID = setInterval(display, 5);
+    let h = 0;
+    function display() {
+      if (h <= 100) {
+        $(".clubs").height(`${(h * 94) / 100}vh`);
+        $(".team").height(`${(h * 47) / 100}vh`);
+        $(".events").height(`${(h * 47) / 100}vh`);
+        $(".banner").width(`${(h * 27) / 100}vw`);
+        $(".media").width(`${(h * 27) / 100}vw`);
+        h++;
       } else {
         clearInterval(intervalID);
       }
     }
   });
-
   $(".dropdown").on("hide.bs.dropdown", () => {
-    banner.style.opacity = 0; //Reseting Banner opacity
+    $(".team").height("0");
+    $(".events").height("0");
+    $(".banner").width("0");
+    $(".media").width("0");
+    $(".clubs").height("0");
   });
 });
